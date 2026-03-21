dd805ef1245c9cbe091d310004bd486fe76106c9
Arjun Subedi <arjunsubedi@live.com>
2026-03-21 18:20:56 +0000

    feat: replace marketing home page with live stats dashboard and quick actions

--- PeaceInternational/Controllers/HomeController.cs
+++ PeaceInternational/Controllers/HomeController.cs
@@ -1,10 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using PeaceInternational.Core.Entity;
+using PeaceInternational.Core.IRepository;
 using PeaceInternational.Models;
 
 namespace PeaceInternational.Controllers
@@ -12,9 +13,43 @@ namespace PeaceInternational.Controllers
     [Authorize]
     public class HomeController : Controller
     {
+        private readonly ICrudService<Customer> _customerService;
+        private readonly ICrudService<Invoice> _invoiceService;
+        private readonly ICrudService<ServiceVoucher> _serviceVoucherService;
+        private readonly ICrudService<Tourcost> _tourcostService;
+
+        public HomeController(
+            ICrudService<Customer> customerService,
+            ICrudService<Invoice> invoiceService,
+            ICrudService<ServiceVoucher> serviceVoucherService,
+            ICrudService<Tourcost> tourcostService)
+        {
+            _customerService = customerService;
+            _invoiceService = invoiceService;
+            _serviceVoucherService = serviceVoucherService;
+            _tourcostService = tourcostService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetStats()
+        {
+            var customers = await _customerService.GetAllAsync();
+            var invoices = await _invoiceService.GetAllAsync();
+            var vouchers = await _serviceVoucherService.GetAllAsync();
+            var tourcosts = await _tourcostService.GetAllAsync();
+
+            return Json(new
+            {
+                customers = customers.Count(),
+                invoices = invoices.Count(),
+                vouchers = vouchers.Count(),
+                tourcosts = tourcosts.Count()
+            });
         }
 
         public IActionResult Privacy()
